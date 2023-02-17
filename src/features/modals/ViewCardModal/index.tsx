688dd9ca2a5b86556f27c68d5d3e4e571f87cd23
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-02-17 14:42:38 +0000

    Add messages

--- src/features/modals/ViewCardModal/index.tsx
+++ src/features/modals/ViewCardModal/index.tsx
@@ -14,6 +14,30 @@ interface Props {
   card: ICard | null;
 }
 
+interface IComment {
+  author: string
+  time: string
+  message: string
+}
+
+const comments: Array<IComment> = [
+  { 
+    author: "Callum Macpherson",
+    time: "15 minutes ago",
+    message: "Blah blah something"
+  },
+  { 
+    author: "Callum Macpherson",
+    time: "15 minutes ago",
+    message: "Blah blah something"
+  },
+  { 
+    author: "Callum Macpherson",
+    time: "15 minutes ago",
+    message: "Blah blah something"
+  }
+]
+
 const ViewCardModal = ({ modalVisible, handleClose, card }: Props) => {
   const [editingDescription, setEditingDescription] = useState<boolean>(false);
 
@@ -144,11 +168,7 @@ const ViewCardModal = ({ modalVisible, handleClose, card }: Props) => {
                         </button>
                       </div>
 
-                      {/* <textarea name="" id=""
                     
-                    // cols="30" rows="10"
-                    //  value={card.description}  
-                     /> */}
                     </div>
                   ) : (
                     <div
@@ -162,6 +182,53 @@ const ViewCardModal = ({ modalVisible, handleClose, card }: Props) => {
                     </div>
                   )}
                 </div>
+
+                <h2 className="text-lg text-gray-800 mb-2 flex items-center mt-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+</svg>
+
+                  <span className="font-medium">Activity</span>
+
+                 
+                 
+                </h2>
+
+                {/* Add Comment */}
+
+                <div className="ml-8 bg-gray-100 p-2 rounded-sm">
+                  <input type="text" name="" id="" placeholder="Write a comment..." className="w-full px-2 py-2 rounded-sm border border-solid border-gray-400 block" />
+                  
+                  <div className="flex gap-x-2">
+
+                  <button className="bg-gray-600 text-gray-50 px-4 py-2 rounded-sm mt-2">Send</button>
+                  <button className="bg-gray-600 text-gray-50 px-4 py-2 rounded-sm mt-2">Cancel</button>
+                  </div>
+                </div>
+
+                <div>
+                <ol className="ml-8 mt-4 flex flex-col gap-y-4">
+{
+  comments.map((comment, index) => (
+<li key={index}>
+    <div className="">
+      <div><span className="font-semibold mr">{comment.author}</span> <span className="font-light text-gray-500">{comment.time}</span></div>
+      <div className="bg-gray-50 rounded-sm p-2 my-1">{comment.message}</div>
+      <div className="flex gap-x-2 font-light underline text-gray-500">
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    </div>
+
+
+</li>
+  ))
+}
+
+</ol>
+                </div>
+
+
               </div>
 
               <div className="">
